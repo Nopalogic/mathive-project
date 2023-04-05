a0fbe911b27aaad35652fa331dfb70877bff7f00
Nopalogic <naufaladhi02@gmail.com>
2023-04-05 21:35:58 +0700

    make custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	darkMode: 'class',
+	content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		container: {
+			center: true,
+			padding: '16px',
+		},
+		extend: {
+			colors: {
+				primary: '#14b8a6',
+				secondary: '#64746b',
+				dark: '#0f172a',
+			},
+		},
+	},
+	plugins: [],
+};
