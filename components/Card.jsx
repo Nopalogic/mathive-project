f4962c46c8f15b909ff9696b868b7b7aa4f5c934
Nopalogic <naufaladhi02@gmail.com>
2023-04-15 11:29:49 +0700

    destructering parameter and add dark theme title

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 
-export default function Card(img, title, category) {
+export default function Card({img, title, category}) {
 	return (
 		<>
 			<figure className="w-72 h-72 grid grid-rows-[1fr,_4rem] overflow-hidden rounded-xl shadow-lg">
 				<Image src={img} alt="" className="w-full h-full object-cover" />
 				<figcaption className="bg-white dark:bg-dark">
 					<p className="px-2 pt-2 text-sm text-secondary">{category}</p>
-					<h3 className="px-2 text-lg font-semibold">{title}</h3>
+					<h3 className="px-2 text-lg font-semibold dark:text-white">{title}</h3>
 				</figcaption>
 			</figure>
 		</>
