2384d8de8d1ac5fe99086e3225cc9366d9a07042
Nopalogic <naufaladhi02@gmail.com>
2023-04-16 22:23:20 +0700

    add Card component

--- app/modul/page.js
+++ app/modul/page.js
@@ -1,6 +1,14 @@
-import Image from "next/image";
+import Card from '@/components/Card';
+import Link from 'next/link';
 
 export default function Modul() {
+	const loops = [
+		{ title: 'matriks', category: 'aljabar linear' },
+		{ title: 'determinan', category: 'aljabar linear' },
+		{ title: 'invers', category: 'aljabar linear' },
+		{ title: 'gauss', category: 'aljabar linear' },
+	];
+
 	return (
 		<>
 			<section className="pt-24 pb-16 bg-slate-200 dark:bg-slate-800">
@@ -10,16 +18,18 @@ export default function Modul() {
 							<h2 className="mb-4 text-3xl font-bold text-dark dark:text-white sm:text-4xl lg:text-5xl">Lastest Project</h2>
 						</div>
 					</div>
-          <div className="w-full px-4">
-            <h2 className="mb-4 text-xl font-bold text-primary">Aljabar Linear</h2>
-            <figure className="w-72 h-72 grid grid-rows-[1fr,_4rem] overflow-hidden rounded-xl shadow-lg">
-								<Image src={''} alt="" className="w-full h-full object-cover" />
-								<figcaption className="bg-white dark:bg-dark">
-                  <p className="px-2 pt-2 text-sm text-secondary">Aljabar Linear</p>
-									<h3 className="px-2 text-lg font-semibold">TItle</h3>
-								</figcaption>
-							</figure>
-          </div>
+					<div className="w-full px-4">
+						<h2 className="mb-4 text-xl font-bold text-primary">Aljabar Linear</h2>
+						<div className="block justify-center gap-7 lg:flex flex-wrap">
+							{loops.map((loop, i) => (
+								<div key={i}>
+									<Link href={'/'}>
+										<Card title={loop.title} category={loop.category} />
+									</Link>
+								</div>
+							))}
+						</div>
+					</div>
 				</div>
 			</section>
 		</>
