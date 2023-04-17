b102421a8f0cadf1df0985b567e7875ef7da722d
Nopalogic <naufaladhi02@gmail.com>
2023-04-17 20:50:36 +0700

    first commit

---
+++ utils/data.js
@@ -0,0 +1,6 @@
+export const modules = [
+  { title: 'matriks', category: 'aljabar linear' },
+  { title: 'determinan', category: 'aljabar linear' },
+  { title: 'invers', category: 'aljabar linear' },
+  { title: 'gauss', category: 'aljabar linear' },
+];
