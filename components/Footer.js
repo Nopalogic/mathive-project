7731ef3f8cf90cccf2381ce9921d627d1fa35c85
Nopalogic <naufaladhi02@gmail.com>
2023-04-18 17:48:17 +0700

    modify

--- components/Footer.js
+++ components/Footer.js
@@ -14,15 +14,6 @@ export default function Footer() {
 					</div>
 
 					<div className="w-full pb-3 lg:w-1/4">
-						<h6 className="my-1 text-base uppercase font-semibold text-neutral-100">Technology</h6>
-						<div className="flex flex-col gap-2">
-							<Link href="https://legacy.reactjs.org/">ReactJS</Link>
-							<Link href="https://nextjs.org/">NextJS</Link>
-							<Link href="https://tailwindcss.com/">Tailwind CSS</Link>
-						</div>
-					</div>
-
-					<div className="w-full pb-3 lg:w-1/4">
 						<h6 className="my-1 text-base uppercase font-semibold text-neutral-100">Quick Links</h6>
 						<div className="flex flex-col gap-2">
 							<Link href="/">Tentang kami</Link>
@@ -30,6 +21,15 @@ export default function Footer() {
 							<Link href="/">Contribute</Link>
 							<Link href="/">Privacy Policy</Link>
 							<Link href="/">Sitemap</Link>
+						</div>
+					</div>
+
+					<div className="w-full pb-3 lg:w-1/4">
+						<h6 className="my-1 text-base uppercase font-semibold text-neutral-100">Technology</h6>
+						<div className="flex flex-col gap-2">
+							<Link href="https://legacy.reactjs.org/">ReactJS</Link>
+							<Link href="https://nextjs.org/">NextJS</Link>
+							<Link href="https://tailwindcss.com/">Tailwind CSS</Link>
 						</div>
 					</div>
 				</div>
